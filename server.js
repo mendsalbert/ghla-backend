77f0251d09ed4eaf66f78273f31e51328d34c961
graphica99 <mendsalbert@gmail.com>
2022-04-20 16:37:31 +0000

    ghla

--- server.js
+++ server.js
@@ -8,13 +8,18 @@ const expressUpload = require("express-fileupload");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const axios = require("axios");
-
+const cron = require("node-cron");
+const Vonage = require("@vonage/server-sdk");
 //@user related routes
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 //@product related routes
 const bookRoute = require("./routes/book");
 
+const vonage = new Vonage({
+  apiKey: "3d4e80f5",
+  apiSecret: "3PKgnHqncyFpha5M",
+});
 //init middleware
 app.use(
   cors({
@@ -37,32 +42,66 @@ app.use("/", (req, res) => {
   res.json({ msg: "home page" });
 });
 
+const sendSMS = (contact) => {
+  str = contact.substring(1);
+  const from = "Ghana Library Authority";
+  const to = "233" + str;
+  console.log(to);
+  const text = "Your book has expired return it for renewal";
+
+  vonage.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (responseData.messages[0]["status"] === "0") {
+        console.log("Message sent successfully.");
+      } else {
+        console.log(
+          `Message failed with error: ${responseData.messages[0]["error-text"]}`
+        );
+      }
+    }
+  });
+};
+//send message
+const sendSMSHandler = () => {
+  axios
+    .get("http://localhost:1000/api/book/all-books-overdue")
+    .then((response) => {
+      console.log(
+        response.data.map((user) => {
+          if (user.user.length <= 0) {
+            return;
+          }
+          user.user.map((overdueuser) => {
+            if (overdueuser.usertype === "adult") {
+              console.log(overdueuser.contact);
+              sendSMS(overdueuser.contact);
+            } else if (overdueuser.usertype === "children") {
+              sendSMS(overdueuser.firstguardiancontact);
+              sendSMS(overdueuser.secondguardiancontact);
+              console.log(overdueuser.firstguardiancontact);
+            }
+          });
+        })
+      );
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 //init database
 connectDB()
   .then((success) => {
     console.log(success);
     app.listen("1000");
+    // 0 0 */3 * * send message every 3 days
+    const task = cron.schedule("1 * * * * *", function () {
+      sendSMSHandler();
+    });
 
-    //*********************
+    task.start();
 
-    axios
-      .get("http://localhost:1000/api/book/all-books-overdue")
-      .then((response) => {
-        console.log(
-          response.data.map((user) => {
-            user.user.map((overdueuser) => {
-              if (overdueuser.usertype === "adult") {
-                console.log(overdueuser.contact);
-              } else if (overdueuser.usertype === "children") {
-                // console.log(overdueuser.firstguardiancontact);
-              }
-            });
-          })
-        );
-      })
-      .catch((error) => {
-        console.log(error);
-      });
     //******************** */
   })
   .catch((e) => {
